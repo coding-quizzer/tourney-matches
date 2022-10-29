1016a4147f3834621d3f1cc1576050f6a758c9af
William Gadd <wgadd@sasktel.net>
2022-10-29 16:36:55 -0600

    Add helper functions to playerList.js and try to prevent addWinsToPlayer from changing the original objects

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,8 +1,13 @@
 import playerData from "../data/playerData";
 import matchData from "../data/matchData";
 import Player from "./Player";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
-const PlayerList = () => {
+const PlayerList = (props) => {
+  console.log(playerData);
+  const playerDataArray = preparePlayerData(playerData);
+  console.log(playerDataArray);
+  console.log(addWinsToPlayers(playerDataArray, matchData));
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -1,7 +1,19 @@
-export const preparePlayerData = () => {
-
+export const preparePlayerData = (playerData) => {
+  return Object.values(playerData);
 };
 
-export const addWinsToPlayers = () => {
+export const addWinsToPlayers = (playerDataArray, matchData) => {
+  return playerDataArray.map(player =>{
+    const playerTag = player.gamerTag;
+    const playerWins =  matchData.reduce((counter, current) => {
+      if(current.winner === playerTag) {
+        return counter + 1;
+      } else {
+        return counter;
+      }
+    }, 0);
 
+    player.wins = playerWins;
+    return player;
+  });
 };
