aeda0966a0bf8508ba42c5a3b1c42bb349daf116
William Gadd <wgadd@sasktel.net>
2022-10-29 15:33:54 -0600

    Added header to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+
+import './App.css';
 import PlayerList from './components/PlayerList';
 import MatchList from './components/MatchList';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <PlayerList />
-      <MatchList />
+      <h1>
+        Tourney Matches{" "} 
+        <span>Where Coding and tournaments found their Match!</span>
+      </h1>
+          <PlayerList />
+          <MatchList />
     </div>
   );
 }
