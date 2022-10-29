6bd433bd54602a219a26a4580059ebadc6d3cc36
William Gadd <wgadd@sasktel.net>
2022-10-29 15:14:37 -0600

    Move Player component inside the PlayerList component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import PlayerList from './components/PlayerList';
-import Player from './components/Player';
 import MatchList from './components/MatchList';
 import Match from './components/Match';
 import './App.css';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <PlayerList />
-      <Player />
       <MatchList />
       <Match />
 
